9898ebad6cc083fca1257567780405f8eae2cb8b
Hanaa Gamal <elshriefhanaa@gmail.com>
2024-05-17 08:15:10 +0300

    create views

--- __manifest__.py
+++ __manifest__.py
@@ -5,6 +5,11 @@
         'views/patient.xml',
         'views/hms_doctors.xml',
         'views/hms_departments.xml',
-    ]
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'hms/static/src/css/custom_styles.css',
+        ],
+    },
 }
 
